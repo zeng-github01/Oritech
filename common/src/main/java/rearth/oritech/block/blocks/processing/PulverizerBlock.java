1f1a21ab3cc7a7c3abd829c942f2c6bb23d29ee6
rearth <davidus2010@gmail.com>
2025-03-07 16:19:16 +0100

    Add entity damage when stepped on pulverizers

--- common/src/main/java/rearth/oritech/block/blocks/processing/PulverizerBlock.java
+++ common/src/main/java/rearth/oritech/block/blocks/processing/PulverizerBlock.java
@@ -1,10 +1,25 @@
 package rearth.oritech.block.blocks.processing;
 
 import net.minecraft.block.BlockEntityProvider;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageEffects;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.damage.DamageSources;
+import net.minecraft.entity.damage.DamageTypes;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 import org.jetbrains.annotations.NotNull;
 import rearth.oritech.block.base.block.UpgradableMachineBlock;
 import rearth.oritech.block.entity.processing.PulverizerBlockEntity;
+import rearth.oritech.init.BlockEntitiesContent;
 
 public class PulverizerBlock extends UpgradableMachineBlock implements BlockEntityProvider {
     
@@ -15,5 +30,19 @@ public class PulverizerBlock extends UpgradableMachineBlock implements BlockEnti
     @Override
     public @NotNull Class<? extends BlockEntity> getBlockEntityType() {
         return PulverizerBlockEntity.class;
+    }
+    
+    @Override
+    public void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity) {
+        
+        if (!world.isClient && entity instanceof LivingEntity livingEntity) {
+            var targetPos = pos.toCenterPos().add(0, 0.5f, 0);
+            var entityDist = entity.squaredDistanceTo(targetPos);
+            if (entityDist > 0.7) return;
+            var isWorking = world.getBlockEntity(pos, BlockEntitiesContent.PULVERIZER_ENTITY).get().progress > 0;
+            if (isWorking)
+                livingEntity.damage(new DamageSource(world.getRegistryManager().get(RegistryKeys.DAMAGE_TYPE).getEntry(DamageTypes.CRAMMING).get()), 1f);
+        }
+        super.onSteppedOn(world, pos, state, entity);
     }
 }
