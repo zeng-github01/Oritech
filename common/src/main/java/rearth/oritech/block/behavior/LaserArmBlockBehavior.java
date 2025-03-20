48d8d710518bc4adfeb47e2bb30d8c227cf038f2
rearth <davidus2010@gmail.com>
2025-03-20 16:37:00 +0100

    Fix enderic laser not perfectly filling energy storages

--- common/src/main/java/rearth/oritech/block/behavior/LaserArmBlockBehavior.java
+++ common/src/main/java/rearth/oritech/block/behavior/LaserArmBlockBehavior.java
@@ -75,7 +75,7 @@ public class LaserArmBlockBehavior {
                     storageCandidate = unstableContainerBlockEntity.laserInputStorage;
                 
                 var insertAmount = storageCandidate.getCapacity() - storageCandidate.getAmount();
-                if (insertAmount < 10)
+                if (insertAmount <= 0)
                     return false;
                 
                 var transferCapacity = Math.min(insertAmount, laserEntity.energyRequiredToFire());
