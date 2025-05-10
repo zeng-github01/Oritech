da4ea654e59871c9de839f241404755286904548
rearth <davidus2010@gmail.com>
2025-05-10 16:43:47 +0200

    Further fluid tank copy steps, add safety check to player augmenter screen

--- common/src/main/java/rearth/oritech/block/blocks/storage/SmallFluidTank.java
+++ common/src/main/java/rearth/oritech/block/blocks/storage/SmallFluidTank.java
@@ -162,7 +162,7 @@ public class SmallFluidTank extends Block implements BlockEntityProvider {
         
         if (itemStack.contains(FluidApi.ITEM.getFluidComponent())) {
             var tankEntity = (SmallTankEntity) world.getBlockEntity(pos);
-            tankEntity.fluidStorage.setStack(itemStack.get(FluidApi.ITEM.getFluidComponent()));
+            tankEntity.fluidStorage.setStack(itemStack.get(FluidApi.ITEM.getFluidComponent()).copy());
         }
     }
     
