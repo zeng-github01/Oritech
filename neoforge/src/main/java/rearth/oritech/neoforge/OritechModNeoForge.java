b82b2d8307cf0a62b17cc4a17c471c1a1e6d5bac
Relentless <relentless@rlnt.dev>
2025-09-15 20:08:16 +0200

    Migrate to Mojang mappings (#501)
    Co-authored-by: rearth <davidus2010@gmail.com>

--- neoforge/src/main/java/rearth/oritech/neoforge/OritechModNeoForge.java
+++ neoforge/src/main/java/rearth/oritech/neoforge/OritechModNeoForge.java
@@ -4,10 +4,10 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.PrimitiveCodec;
 import dev.architectury.fluid.FluidStack;
 import dev.architectury.hooks.fluid.forge.FluidStackHooksForge;
-import net.minecraft.component.ComponentType;
-import net.minecraft.network.codec.PacketCodecs;
-import net.minecraft.registry.RegistryKeys;
-import net.minecraft.util.math.BlockPos;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.component.DataComponentType;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.codec.ByteBufCodecs;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
@@ -19,12 +19,10 @@ import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.neoforged.neoforge.registries.RegisterEvent;
 import rearth.oritech.Oritech;
-import rearth.oritech.api.attachment.neoforge.AttachmentApiImpl;
 import rearth.oritech.api.energy.EnergyApi;
 import rearth.oritech.api.fluid.FluidApi;
 import rearth.oritech.api.item.ItemApi;
 import rearth.oritech.api.networking.NetworkManager;
-import rearth.oritech.api.networking.neoforge.NetworkManagerImpl;
 import rearth.oritech.item.tools.util.ArmorEventHandler;
 
 @Mod(Oritech.MOD_ID)
@@ -39,7 +37,7 @@ public final class OritechModNeoForge {
         eventBus.register(new EventHandler());
         EventHandler.COMPONENT_REGISTRAR.register(eventBus);
         
-        AttachmentApiImpl.ATTACHMENT_TYPES.register(eventBus);
+        OritechPlatformNeoForge.ATTACHMENT_TYPES.register(eventBus);
         
         fluidApiInstance = new NeoforgeFluidApiImpl();
         FluidApi.BLOCK = fluidApiInstance;
@@ -53,7 +51,7 @@ public final class OritechModNeoForge {
         EnergyApi.ITEM = energyApiInstance;
         
         NetworkManager.FLUID_STACK_CODEC = net.neoforged.neoforge.fluids.FluidStack.OPTIONAL_CODEC.xmap(FluidStackHooksForge::fromForge, FluidStackHooksForge::toForge);
-        NetworkManager.FLUID_STACK_STREAM_CODEC = net.neoforged.neoforge.fluids.FluidStack.OPTIONAL_STREAM_CODEC.xmap(FluidStackHooksForge::fromForge, FluidStackHooksForge::toForge);
+        NetworkManager.FLUID_STACK_STREAM_CODEC = net.neoforged.neoforge.fluids.FluidStack.OPTIONAL_STREAM_CODEC.map(FluidStackHooksForge::fromForge, FluidStackHooksForge::toForge);
         
         Oritech.initialize();
         
@@ -72,26 +70,26 @@ public final class OritechModNeoForge {
     class EventHandler {
         
         // see ComponentContent.java for why this is incredibly stupid but required
-        public static final DeferredRegister.DataComponents COMPONENT_REGISTRAR = DeferredRegister.createDataComponents(RegistryKeys.DATA_COMPONENT_TYPE, Oritech.MOD_ID);
+        public static final DeferredRegister.DataComponents COMPONENT_REGISTRAR = DeferredRegister.createDataComponents(Registries.DATA_COMPONENT_TYPE, Oritech.MOD_ID);
         
-        public static final DeferredHolder<ComponentType<?>, ComponentType<Boolean>> IS_AOE_ACTIVE = COMPONENT_REGISTRAR.registerComponentType(
+        public static final DeferredHolder<DataComponentType<?>, DataComponentType<Boolean>> IS_AOE_ACTIVE = COMPONENT_REGISTRAR.registerComponentType(
           "is_aoe_active",
-          builder -> builder.codec(PrimitiveCodec.BOOL).packetCodec(PacketCodecs.BOOL)
+          builder -> builder.persistent(PrimitiveCodec.BOOL).networkSynchronized(ByteBufCodecs.BOOL)
         );
         
-        public static final DeferredHolder<ComponentType<?>, ComponentType<BlockPos>> TARGET_POSITION = COMPONENT_REGISTRAR.registerComponentType(
+        public static final DeferredHolder<DataComponentType<?>, DataComponentType<BlockPos>> TARGET_POSITION = COMPONENT_REGISTRAR.registerComponentType(
           "target_position",
-          builder -> builder.codec(BlockPos.CODEC).packetCodec(BlockPos.PACKET_CODEC)
+          builder -> builder.persistent(BlockPos.CODEC).networkSynchronized(BlockPos.STREAM_CODEC)
         );
         
-        public static final DeferredHolder<ComponentType<?>, ComponentType<FluidStack>> STORED_FLUID = COMPONENT_REGISTRAR.registerComponentType(
+        public static final DeferredHolder<DataComponentType<?>, DataComponentType<FluidStack>> STORED_FLUID = COMPONENT_REGISTRAR.registerComponentType(
           "stored_fluid",
-          builder -> builder.codec(FluidStack.CODEC).packetCodec(FluidStack.STREAM_CODEC)
+          builder -> builder.persistent(FluidStack.CODEC).networkSynchronized(FluidStack.STREAM_CODEC)
         );
         
-        public static final DeferredHolder<ComponentType<?>, ComponentType<Long>> NEO_ENERGY_COMPONENT = COMPONENT_REGISTRAR.registerComponentType(
+        public static final DeferredHolder<DataComponentType<?>, DataComponentType<Long>> NEO_ENERGY_COMPONENT = COMPONENT_REGISTRAR.registerComponentType(
           "energy",
-          builder -> builder.codec(Codec.LONG).packetCodec(PacketCodecs.VAR_LONG)
+          builder -> builder.persistent(Codec.LONG).networkSynchronized(ByteBufCodecs.VAR_LONG)
         );
         
         @SubscribeEvent
@@ -104,7 +102,7 @@ public final class OritechModNeoForge {
         @SubscribeEvent
         public void register(RegisterEvent event) {
             
-            var id = event.getRegistryKey().getValue();
+            var id = event.getRegistryKey().location();
             
             if (Oritech.EVENT_MAP.containsKey(id)) {
                 Oritech.LOGGER.debug(event.getRegistryKey().toString());
@@ -117,15 +115,15 @@ public final class OritechModNeoForge {
         public void register(final RegisterPayloadHandlersEvent event) {
             var registrar = event.registrar("1");
             
-            for (var toInit : NetworkManagerImpl.PENDING_S2C_INITS) {
+            for (var toInit : OritechPlatformNeoForge.PENDING_S2C_INITS) {
                 toInit.accept(registrar);
             }
-            NetworkManagerImpl.PENDING_S2C_INITS.clear();
+            OritechPlatformNeoForge.PENDING_S2C_INITS.clear();
             
-            for (var toInit : NetworkManagerImpl.PENDING_C2S_INITS) {
+            for (var toInit : OritechPlatformNeoForge.PENDING_C2S_INITS) {
                 toInit.accept(registrar);
             }
-            NetworkManagerImpl.PENDING_C2S_INITS.clear();
+            OritechPlatformNeoForge.PENDING_C2S_INITS.clear();
             
         }
         
